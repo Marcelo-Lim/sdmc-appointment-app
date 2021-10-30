af9b0ca3b035112bc5a879b61feb26e50cd08fcb
Marcelo-Lim <lim.022360@cubao.sti.edu.ph>
2021-10-31 01:53:32 +0800

    added layout for booking module

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,13 +1,13 @@
-import { Typography, makeStyles } from "@material-ui/core";
+import { Typography, makeStyles, TextField } from "@material-ui/core";
+import Autocomplete from '@mui/material/Autocomplete';
+
 import React from "react";
 
 const ContactUs = () => {
 const classes = useStyles();
   return (
     <div>
-      <div className="heading">
-        <Typography className={classes.typo1}>Contact Us</Typography>
-      </div>
+      <Typography className={classes.typo1}>Contact Us</Typography>
     </div>
   )
 };
